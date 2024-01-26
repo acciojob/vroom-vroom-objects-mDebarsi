210cf0e6f7fe3bb0d068aaa5686edaf6f271bfbc
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2024-01-26 19:19:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	 this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Add getMakeModel method to Car prototype
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+	// Call the Car constructor with 'this' context
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// Inherit the Car prototype in the SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Add getTopSpeed method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
